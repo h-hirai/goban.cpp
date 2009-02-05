0cbc36927a4ce972efb7862715a9254a403966ba
HIRAI, Hajime <hirai.hajime@gmail.com>
2009-02-05 11:36:54 +0900

    see diff

--- goban.cpp
+++ goban.cpp
@@ -22,22 +22,9 @@ bool Point::operator<(const Point p) const {
   return row < p.row || (row == p.row && col < p.col);
 }
 
-Board::Board(int size) : size(size) {
-  brd_state = new color_t*[size];
-  for (int row = 0; row < size; row++) {
-    brd_state[row] = new color_t[size];
-    for (int col = 0; col < size; col++) {
-      brd_state[row][col] = empty;
-    }
-  }
-}
-
-Board::~Board() {
-  for (int row = 0; row < size; row ++) {
-    delete[] brd_state[row];
-  }
-  delete[] brd_state;
-}
+Board::Board(int size) :
+  size(size),
+  brd_state(vector<vector<color_t> >(size, vector<color_t>(size, empty))) {}
 
 color_t Board::ref(Point p) {
   return brd_state[p.row][p.col];
